182e3131ea4ca561607ed271b64fdb69eabc564e
Arvin <arvinpaulmanila@gmail.com>
2022-04-11 15:13:20 -0700

    testing a commit -AM

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // just checking if this git thing works -Arvin
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
